7f106f6001415ecad2c4a8c4571b55e792d52c21
shmoops262 <124469682+shmoops262@users.noreply.github.com>
2025-11-02 20:06:32 -0500

    first try

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,27 @@
+module.exports = function(eleventyConfig) {
+  // Passthrough static assets
+  eleventyConfig.addPassthroughCopy("images");
+  eleventyConfig.addPassthroughCopy("styles");
+
+  // Liquid shortcode to render a small item card
+  eleventyConfig.addLiquidShortcode("itemCard", function(title, img, shortDesc, why, url) {
+    return `<article class="card">
+      <img class="item-image" src="${img}" alt="${title}" />
+      <h2>${title}</h2>
+      <p class="short-desc">${shortDesc}</p>
+      <p class="why">${why}</p>
+      <a class="more" href="${url}">Read more →</a>
+    </article>`;
+  });
+
+  return {
+    dir: {
+      input: ".",
+      includes: "_includes",
+      output: "_site"
+    }
+  };
+};
 
 // The export statement makes these settings available to other files in 11ty
 module.exports = function(eleventyConfig) {
--- README.md
+++ README.md
@@ -8,3 +8,12 @@ Super basic [11ty](https://www.11ty.dev) starter to get you moving in the right 
 2. Open the new repository in Codespaces or your own development environment.
 3. Make sure you have Node.js installed (this will be automatic if using Codespaces).
 4. In your terminal type `npx @11ty/eleventy --serve` to build and view the template.
+
+## How to run this site
+
+Install dependencies and run the dev server locally:
+
+1. npm install
+2. npm start
+
+Then visit http://localhost:8080 to see the site. The site uses a Liquid shortcode `itemCard` defined in `.eleventy.js` and a shared layout in `_includes/layout.liquid`.
